d8e3c21415726671a01352347a3c0a63f0f8f8b4
Viniciusasn30 <Viniciusasnascimento@gmail.com>
2023-10-10 10:08:05 -0300

    Mod5

---
+++ src/br/com/serrano/PrimeiraClasse.java
@@ -0,0 +1,13 @@
+package br.com.serrano;
+
+public class PrimeiraClasse{
+	
+	public static void main(String[] args) {
+		String ambiente =args[0];
+		System.out.print(ambiente);
+	
+	System.out.print(" VInicius Aragão");
+	
+	}
+	
+}
